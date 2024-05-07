2f1c81f709ffc517596a430a5a4b8ff659f6717b
Sachin <sachin@Sachins-MacBook-Air.local>
2024-05-08 00:06:44 +0530

    fix bugs

--- src/app/tutor-infomation/page.js
+++ src/app/tutor-infomation/page.js
@@ -20,72 +20,72 @@ import Priya from "@/assets/Tutor/Priya.jpeg"
 import Radhika from "@/assets/Tutor/Radhika.jpeg"
 import Shele from "@/assets/Tutor/Shele.jpeg"
 function TutorDetails() {
-    const [tab,setTab]=useState(1)
-    const [link,setLink]=useState()
-  const [vedioModal,setVedioModal] = useState(false)
+    const [tab, setTab] = useState(1)
+    const [link, setLink] = useState()
+    const [vedioModal, setVedioModal] = useState(false)
     console.log(teacherInformation);
 
-    let respnse=teacherInformation?.teacherDetails
+    let respnse = teacherInformation?.teacherDetails
 
     const FaqVedios = [
         {
-          url: "https://englishyaarilp.s3.ap-south-1.amazonaws.com/7th+Sept+Megha.mp4",
-          title: "How to use EnglishYaari App? How EnglishYaari works?",
-          name:'Megha Nayak',
-          sessionTaken:'971',
-          img:Megha
-    
+            url: "https://englishyaarilp.s3.ap-south-1.amazonaws.com/7th+Sept+Megha.mp4",
+            title: "How to use EnglishYaari App? How EnglishYaari works?",
+            name: 'Megha Nayak',
+            sessionTaken: '971',
+            img: Megha
+
         },
-    
+
         {
-          url: "https://www.youtube.com/watch?v=z6c836gUoeo&t=6s",
-          title: "How to use EnglishYaari App? How EnglishYaari works?",
-          name:'Madhav Anand',
-          sessionTaken:'1115',
-          img:Bhaubali
-    
+            url: "https://www.youtube.com/watch?v=z6c836gUoeo&t=6s",
+            title: "How to use EnglishYaari App? How EnglishYaari works?",
+            name: 'Madhav Anand',
+            sessionTaken: '1115',
+            img: Bhaubali
+
         },
-    
+
         {
-          url: "https://englishyaarilp.s3.ap-south-1.amazonaws.com/7th+Sept+Megha.mp4",
-          title: "How to use EnglishYaari App? How EnglishYaari works?",
-          name:'Moupriya Mukherjee',
-          sessionTaken:'450',
-          img:Priya
-    
+            url: "https://englishyaarilp.s3.ap-south-1.amazonaws.com/7th+Sept+Megha.mp4",
+            title: "How to use EnglishYaari App? How EnglishYaari works?",
+            name: 'Moupriya Mukherjee',
+            sessionTaken: '450',
+            img: Priya
+
         },
-     
+
         {
-          url: "https://englishyaarilp.s3.ap-south-1.amazonaws.com/7th+Sept+Amal.mp4",
-          title: "How to Subscribe a Plan on EnglishYaari App?",
-          name:'Pritam Pradhan',
-          sessionTaken:'950',
-          img:Pritam
-    
+            url: "https://englishyaarilp.s3.ap-south-1.amazonaws.com/7th+Sept+Amal.mp4",
+            title: "How to Subscribe a Plan on EnglishYaari App?",
+            name: 'Pritam Pradhan',
+            sessionTaken: '950',
+            img: Pritam
+
         },
-    
+
         {
-          url: "https://englishyaarilp.s3.ap-south-1.amazonaws.com/7th+Sept+Sarika.mp4",
-          title: "How to Join your upcoming Session on EnglishYaari App? ",
-          name:'Radhika Bansal',
-          sessionTaken:'500',
-          img:Radhika
-    
+            url: "https://englishyaarilp.s3.ap-south-1.amazonaws.com/7th+Sept+Sarika.mp4",
+            title: "How to Join your upcoming Session on EnglishYaari App? ",
+            name: 'Radhika Bansal',
+            sessionTaken: '500',
+            img: Radhika
+
         },
-    
-    
+
+
         {
-          url: "https://englishyaarilp.s3.ap-south-1.amazonaws.com/7th+Sept+Sarika.mp4",
-          title: "How to Join your upcoming Session on EnglishYaari App? ",
-          name:'Shailee Shah',
-          sessionTaken:'500',
-          img:Shele
-    
+            url: "https://englishyaarilp.s3.ap-south-1.amazonaws.com/7th+Sept+Sarika.mp4",
+            title: "How to Join your upcoming Session on EnglishYaari App? ",
+            name: 'Shailee Shah',
+            sessionTaken: '500',
+            img: Shele
+
         },
-    
-      ];
+
+    ];
     return (
-        <div style={{overflow:'hidden'}}  className="py-20 bg-background min-h-screen  overflow-hidden">
+        <div style={{ overflow: 'hidden' }} className="py-20 bg-background min-h-screen  overflow-hidden">
             <NavBar2 />
             <div className="container flex flex-wrap-reverse justify-between !mt-10 ">
                 <div className=" w-[90%] mx-auto lg:mx-0 lg:w-[60%]">
@@ -112,11 +112,11 @@ function TutorDetails() {
                 <div className="flex  ">
                     {["Availability", "Reviews",].map((ele, ind) => {
                         return (
-                            <button 
-                            onClick={()=>{
-                                setTab(ind+1)
-                            }}
-                            key={ind} className={twMerge("px-4 font-semibold",ind+1==tab?'border-b-2 text-primary-500 border-primary-500':'')}>
+                            <button
+                                onClick={() => {
+                                    setTab(ind + 1)
+                                }}
+                                key={ind} className={twMerge("px-4 font-semibold", ind + 1 == tab ? 'border-b-2 text-primary-500 border-primary-500' : '')}>
                                 {ele}
                             </button>
                         );
@@ -124,9 +124,9 @@ function TutorDetails() {
                 </div>
 
                 <div className=" w-[90%]  lg:w-[60%] ">
-                   {tab==1 && <Availability />}
+                    {tab == 1 && <Availability />}
 
-                  { tab==2 && <Reviews />}
+                    {tab == 2 && <Reviews />}
 
 
 
@@ -138,13 +138,13 @@ function TutorDetails() {
 
 
 
-      <div className=" h-[400px] w-[90vw]  overflow-x-hidden  margin24">
+                    <div className=" h-[400px] w-[90vw]  overflow-x-hidden  margin24">
 
-      <TeacherSlider setLink={setLink} setVedioModal={setVedioModal} buttonShow={true} images={FaqVedios} speed={40000}/>
+                        <TeacherSlider setLink={setLink} setVedioModal={setVedioModal} buttonShow={true} images={FaqVedios} speed={40000} />
 
-      </div>
+                    </div>
 
-              
+
                 </div>
             </div>
 
@@ -279,7 +279,7 @@ const Availability = () => {
             </div>
 
             <div className="h-full absolute w-full top-0 left-0  flex justify-center items-center backdrop-blur-sm bg-white/30">
-                <button className={twMerge(" text18 text-white flex items-center gap-3 px-6 rounded-full py-2 bg-primary-500", )}>
+                <button className={twMerge(" text18 text-white flex items-center gap-3 px-6 rounded-full py-2 bg-primary-500",)}>
                     Login to see slot availability
                     <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M16.4199 6.4125H13.7574V4.375C13.7574 3.42354 13.3795 2.51104 12.7067 1.83825C12.0339 1.16547 11.1214 0.7875 10.1699 0.7875C9.21846 0.7875 8.30596 1.16547 7.63318 1.83825C6.96039 2.51104 6.58242 3.42354 6.58242 4.375V6.4125H3.91992C3.6315 6.4125 3.35489 6.52708 3.15094 6.73102C2.947 6.93497 2.83242 7.21158 2.83242 7.5V16.25C2.83242 16.5384 2.947 16.815 3.15094 17.019C3.35489 17.2229 3.6315 17.3375 3.91992 17.3375H16.4199C16.7083 17.3375 16.985 17.2229 17.1889 17.019C17.3928 16.815 17.5074 16.5384 17.5074 16.25V7.5C17.5074 7.21158 17.3928 6.93497 17.1889 6.73102C16.985 6.52708 16.7083 6.4125 16.4199 6.4125ZM7.50742 4.375C7.50742 3.66886 7.78793 2.99164 8.28725 2.49233C8.78657 1.99301 9.46378 1.7125 10.1699 1.7125C10.8761 1.7125 11.5533 1.99301 12.0526 2.49233C12.5519 2.99164 12.8324 3.66886 12.8324 4.375V6.4125H7.50742V4.375ZM16.5824 16.25C16.5824 16.2931 16.5653 16.3344 16.5348 16.3649C16.5044 16.3954 16.463 16.4125 16.4199 16.4125H3.91992C3.87682 16.4125 3.83549 16.3954 3.80502 16.3649C3.77454 16.3344 3.75742 16.2931 3.75742 16.25V7.5C3.75742 7.4569 3.77454 7.41557 3.80502 7.38509C3.83549 7.35462 3.87682 7.3375 3.91992 7.3375H16.4199C16.463 7.3375 16.5044 7.35462 16.5348 7.3851C16.5653 7.41557 16.5824 7.4569 16.5824 7.5V16.25ZM10.8143 12.3056C10.8995 12.1781 10.9449 12.0283 10.9449 11.875C10.9449 11.6695 10.8633 11.4723 10.7179 11.327C10.5726 11.1817 10.3755 11.1 10.1699 11.1C10.0166 11.1 9.8668 11.1455 9.73936 11.2306C9.61191 11.3158 9.51257 11.4368 9.45392 11.5784C9.39526 11.72 9.37991 11.8759 9.40981 12.0262C9.43972 12.1765 9.51353 12.3146 9.62191 12.423C9.7303 12.5314 9.86839 12.6052 10.0187 12.6351C10.1691 12.665 10.3249 12.6497 10.4665 12.591C10.6081 12.5323 10.7292 12.433 10.8143 12.3056Z" fill="#EEECF8" stroke="#EEECF8" stroke-width="0.3" />
@@ -336,7 +336,7 @@ const Reviews = () => {
                 }
 
                 <div className="h-full absolute w-full  flex justify-center items-center backdrop-blur-sm bg-white/30">
-                    <button className={twMerge(" text18 text-white flex items-center gap-3 px-6 rounded-full py-2 bg-primary-500", )}>
+                    <button className={twMerge(" text18 text-white flex items-center gap-3 px-6 rounded-full py-2 bg-primary-500",)}>
                         Login to see reviews
                         <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M16.4199 6.4125H13.7574V4.375C13.7574 3.42354 13.3795 2.51104 12.7067 1.83825C12.0339 1.16547 11.1214 0.7875 10.1699 0.7875C9.21846 0.7875 8.30596 1.16547 7.63318 1.83825C6.96039 2.51104 6.58242 3.42354 6.58242 4.375V6.4125H3.91992C3.6315 6.4125 3.35489 6.52708 3.15094 6.73102C2.947 6.93497 2.83242 7.21158 2.83242 7.5V16.25C2.83242 16.5384 2.947 16.815 3.15094 17.019C3.35489 17.2229 3.6315 17.3375 3.91992 17.3375H16.4199C16.7083 17.3375 16.985 17.2229 17.1889 17.019C17.3928 16.815 17.5074 16.5384 17.5074 16.25V7.5C17.5074 7.21158 17.3928 6.93497 17.1889 6.73102C16.985 6.52708 16.7083 6.4125 16.4199 6.4125ZM7.50742 4.375C7.50742 3.66886 7.78793 2.99164 8.28725 2.49233C8.78657 1.99301 9.46378 1.7125 10.1699 1.7125C10.8761 1.7125 11.5533 1.99301 12.0526 2.49233C12.5519 2.99164 12.8324 3.66886 12.8324 4.375V6.4125H7.50742V4.375ZM16.5824 16.25C16.5824 16.2931 16.5653 16.3344 16.5348 16.3649C16.5044 16.3954 16.463 16.4125 16.4199 16.4125H3.91992C3.87682 16.4125 3.83549 16.3954 3.80502 16.3649C3.77454 16.3344 3.75742 16.2931 3.75742 16.25V7.5C3.75742 7.4569 3.77454 7.41557 3.80502 7.38509C3.83549 7.35462 3.87682 7.3375 3.91992 7.3375H16.4199C16.463 7.3375 16.5044 7.35462 16.5348 7.3851C16.5653 7.41557 16.5824 7.4569 16.5824 7.5V16.25ZM10.8143 12.3056C10.8995 12.1781 10.9449 12.0283 10.9449 11.875C10.9449 11.6695 10.8633 11.4723 10.7179 11.327C10.5726 11.1817 10.3755 11.1 10.1699 11.1C10.0166 11.1 9.8668 11.1455 9.73936 11.2306C9.61191 11.3158 9.51257 11.4368 9.45392 11.5784C9.39526 11.72 9.37991 11.8759 9.40981 12.0262C9.43972 12.1765 9.51353 12.3146 9.62191 12.423C9.7303 12.5314 9.86839 12.6052 10.0187 12.6351C10.1691 12.665 10.3249 12.6497 10.4665 12.591C10.6081 12.5323 10.7292 12.433 10.8143 12.3056Z" fill="#EEECF8" stroke="#EEECF8" stroke-width="0.3" />
